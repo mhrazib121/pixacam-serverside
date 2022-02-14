b28db9057e423f464449668ab2b8ab1635a17a94
mhrazib121 <mhrazib121@gmail.com>
2022-02-14 18:21:29 +0600

    update

--- index.js
+++ index.js
@@ -103,6 +103,35 @@ async function run(){
             res.send(user)
         })
 
+        // Admin
+        app.get('/users/:email', async (req, res) => {
+            const email = req.params.email;
+            console.log(email)
+            const query = { email: email };
+            const user = await allUserCollection.findOne(query);
+            let isAdmin = false;
+            if (user?.role === 'admin') {
+                isAdmin = true;
+            }
+            res.json({ admin: isAdmin })
+        })
+
+        app.put('/users', async (req, res) => {
+            const user = req.body;
+            const filter = { email: user.email };
+            const options = { upsert: true };
+            const updateDoc = { $set: user };
+            const result = await allUserCollection.updateOne(filter, updateDoc, options);
+            res.json(result)
+        })
+        app.put('/users/admin', async (req, res) => {
+            const user = req.body;
+            const filter = { email: user.email };
+            const updateDoc = { $set: { role: 'admin' } };
+            const result = await allUserCollection.updateOne(filter, updateDoc);
+            res.json(result);
+        })
+
     }
     finally{
 
