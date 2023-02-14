5b5abd3be4a1bf39de60e952f099bdac0dfcb374
Inmogr-Razib <tmp.mukbul.hassan@inmogr.com>
2023-02-14 16:43:43 +0600

    removed

--- index.js
+++ index.js
@@ -135,7 +135,7 @@ async function run() {
             res.json(result);
         });
 
-        // Delete edit
+        // Delete removed
 
         app.delete('/productscard/:id', async (req, res) => {
             const id = req.params.id;
