c98818e7145014efd244e93afc93660fee5f6e88
razib121 <mhrazib121@gmail.com>
2023-02-27 20:25:33 +0600

    updated

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
 const { MongoClient, ObjectId } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
+const app = express();
 // const res = require('express/lib/response');
 
-const app = express();
-app.use(cors())
+// app.use(cors())
+// app.use(express.json());
+
+//Middleware
+//middleware
 app.use(express.json());
-require('dotenv').config()
+app.use(cors());
+const corsConfig = {
+    origin: "*",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+};
+app.use(cors(corsConfig));
+app.options("*", cors(corsConfig));
+app.use(express.json());
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept,authorization"
+    );
+    next();
+});
 
 const port = process.env.PORT || 5001;
 
@@ -16,7 +37,7 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 
 async function run() {
     try {
-        await client.connect();
+        client.connect();
         const database = client.db('pixacam');
         const allProductCollection = database.collection("allProduct");
         const allOrderCollection = database.collection("allOrder");
