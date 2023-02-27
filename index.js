aa97d27e488cf7e682d731dea8f54dae45fd8b4f
razib121 <mhrazib121@gmail.com>
2023-02-27 20:39:35 +0600

    updated

--- index.js
+++ index.js
@@ -2,32 +2,11 @@ const { MongoClient, ObjectId } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const app = express();
 // const res = require('express/lib/response');
 
-// app.use(cors())
-// app.use(express.json());
-
-//Middleware
-//middleware
+const app = express();
+app.use(cors())
 app.use(express.json());
-app.use(cors());
-const corsConfig = {
-    origin: "*",
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE"],
-};
-app.use(cors(corsConfig));
-app.options("*", cors(corsConfig));
-app.use(express.json());
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept,authorization"
-    );
-    next();
-});
 
 const port = process.env.PORT || 5001;
 
