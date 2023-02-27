4fb2d2b8815bf21a4308a65c7e24203fc3d16e31
razib121 <mhrazib121@gmail.com>
2023-02-27 22:15:33 +0600

    modify

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ app.use(express.json());
 
 const port = process.env.PORT || 5001;
 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*")
+})
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xvulc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
